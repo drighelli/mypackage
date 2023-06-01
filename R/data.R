0d51e05d0d7be6d19cb69ea81f0589764eda73c4
Dario <dario.righelli@gmail.com>
2023-06-01 16:55:39 +0200

    fixing documentation for subcars

--- R/data.R
+++ R/data.R
@@ -4,8 +4,8 @@
 #'
 #' @format a dataframe of 38 observations and 2 variables
 #' \describe{
-#' \item{speed}{djksaljdlsa}
-#' \item{dist}{sdaskjdasl}
+#' \item{speed}{see speed of base cars dataset}
+#' \item{dist}{see dist of base cars dataset}
 #' }
 #' @source the cars dataset
 #'
